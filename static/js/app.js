11add3e94c3235c5572fcc41083c9c405bec76fc
John Mowry <72773479+JohnMowry77@users.noreply.github.com>
2021-03-29 20:19:25 -0500

    read json samples

---
+++ static/js/app.js
@@ -0,0 +1,18 @@
+
+
+
+ d3.json("samples.json").then((samples)=> {
+	console.log(samples)
+
+	// var names =samples. 
+	// //Creat the Traces
+	// var trace1= {
+	// 	x:
+	// }
+});
+
+// var data = [trace1];
+
+
+
+// Plotly.newPLot("bar", data, layout )
