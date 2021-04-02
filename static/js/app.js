63c88c920082f77478d66ea2645d9a19b26c5cc8
John Mowry <72773479+JohnMowry77@users.noreply.github.com>
2021-04-01 23:02:35 -0500

    updates after office hours

--- static/js/app.js
+++ static/js/app.js
@@ -11,130 +11,140 @@ function updatePage() {
   // Use d3 to read in samples.json
   d3.json('samples.json').then((data)=> {
   // Use d3 to select the dropdown menu
-  var dropdownMenu = d3.selectAll("#selDataset"); //# means id // . means class
-  //use forEach grab the names key 
-  data['names'].forEach((id_num)=> {
-  //append (the name text to value name in the dropdown menu)
-  dropdownMenu.append('option').text(id_num).property('value', id_num);	//.text method grab (name text)
+    var dropdownMenu = d3.select("#selDataset"); //# means id // . means class
+      //use forEach grab the names key 
+      data['names'].forEach((id_num)=> {
+      //append (the name text to value name in the dropdown menu)
+        dropdownMenu.append('option').text(id_num).property('value', id_num);	//.text method grab (name text)
+    });
   });
-});
 }
+
 updatePage();
 
 function buildPlot(sample_id){
   d3.json('samples.json').then((data)=>{
-  //create a filter to pull the sample_id ????????FOLLOW UP NEEDED CHART NOT CHANGING
-  //x=>x.id provides place holder to compare to sample_id
-  var result_filter= data.samples.filter(x=>x.id==sample_id)[0]
-  // console.log(sample_id)
-  //data is an array of objects. use samples key, index 0, grab otu_labels key
+    //create a filter to pull the sample_id ????????FOLLOW UP NEEDED CHART NOT CHANGING
+    //x=>x.id provides place holder to compare to sample_id
+    var result_filter= data.samples.filter(x=>x.id==sample_id)[0]
+    // console.log(sample_id)
+    //data is an array of objects. use samples key, index 0, grab otu_labels key
 
-  //use result_filter for bar chart:
-  // var otu_ids_bar=data.samples['otu_ids']
-  var metadata=data['metadata']; 
-  // console.log(metadata) //metadata an array of objects
-  // var samples= data['samples'];
-  // console.log(samples); //samples an array of objects
+    //use result_filter for bar chart:
+    // var otu_ids_bar=data.samples['otu_ids']
+    var metadata=data['metadata'].filter(x=>x.id==sample_id)[0]; 
+    // console.log(metadata) //metadata an array of objects
+    // var samples= data['samples'];
+    // console.log(samples); //samples an array of objects
 
-  //use data.samples for bubble chart
-  var otu_ids=data.samples[0]['otu_ids']
-  // console.log(otu_ids);
-  //use samples key, index 0, grab samples_values key
-  var samples = data.samples[0]['sample_values']
-  // console.log(samples)
-  //use samples key, index 0, grab otu_labels
-  var hover_text=data.samples[0]['otu_labels']
-  // console.log(hover_text);
-  
-  // convert otu_ids to string
-  otu_label= otu_ids.map(x=> `otu ${x}`).slice(0,10).reverse()
+    //use data.samples for bubble chart
+    // data.samples[0]
+    var otu_ids=result_filter['otu_ids']
+    // console.log(otu_ids);
+    //use samples key, index 0, grab samples_values key
+    var samples = result_filter['sample_values']
+    // console.log(samples)
+    //use samples key, index 0, grab otu_labels
+    var hover_text=result_filter['otu_labels']
+    // console.log(hover_text);
+    
+    // convert otu_ids to string
+    otu_label= otu_ids.map(x=> `otu ${x}`).slice(0,10).reverse()
 
-//create trace & use .slice & .reverse to get the top 10 results
-var trace_bar= {
-  'type': 'bar',
-  'y': otu_label,
-  'x': samples.slice(0,10).reverse(),
-  'text': hover_text.slice(0,10).reverse(),
-  'orientation': 'h'
-};
-// console.log(otu_ids.slice(0,10).reverse());
-// // console.log(otu_ids.slice(0,10));
-// console.log(samples.slice(0,10).reverse());
-// console.log(hover_text.slice(0,10).reverse());
+    //create trace & use .slice & .reverse to get the top 10 results
+    var trace_bar= {
+      'type': 'bar',
+      'y': otu_label,
+      'x': samples.slice(0,10).reverse(),
+      'text': hover_text.slice(0,10).reverse(),
+      'orientation': 'h'
+    };
+    // console.log(otu_ids.slice(0,10).reverse());
+    // // console.log(otu_ids.slice(0,10));
+    // console.log(samples.slice(0,10).reverse());
+    // console.log(hover_text.slice(0,10).reverse());
 
-var bar_layout= {
-  'title': 'Top 10 Clusters Found',
-//   'height': 30,
-//   'width': 30
-};
+    var bar_layout= {
+      'title': 'Top 10 Clusters Found',
+    //   'height': 30,
+    //   'width': 30
+    };
 
-//create plot using Plotly: use 'bar' as type & trace as data, plus a layout
-Plotly.newPlot('bar', [trace_bar], bar_layout);
+    //create plot using Plotly: use 'bar' as type & trace as data, plus a layout
+    Plotly.newPlot('bar', [trace_bar], bar_layout);
 
-// var sample_values= 
-var trace_bubble= {
-  'type': 'scatter',
-  'y': otu_ids,
-  'x': samples,
-  mode: 'markers',
-  text: hover_text,
-  marker: {
-    // color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-    // // opacity: [1, 0.8, 0.6, 0.4],
-    size: samples,
-    color: otu_ids
-  }
-};
-var bubble_layout= {
-  xaxis: {title:{text: 'OTU ID'}}
-};
+    // var sample_values= 
+    var trace_bubble= {
+      'type': 'scatter',
+      'y': otu_ids,
+      'x': samples,
+      mode: 'markers',
+      text: hover_text,
+      marker: {
+        // color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+        // // opacity: [1, 0.8, 0.6, 0.4],
+        size: samples,
+        color: otu_ids
+      }
+    };
+    var bubble_layout= {
+      xaxis: {title:{text: 'OTU ID'}}
+    };
 
-Plotly.newPlot('bubble', [trace_bubble], bubble_layout);
+    Plotly.newPlot('bubble', [trace_bubble], bubble_layout);
 
-//Create Data Table
-//Get a refernce to the table body
-var tbody=d3.select("tbody");
-// var cur_metadata=metadata.filter(x=>x.id==sample_id);
-// console.log(cur_metadata);
-Object.entries(metadata[0]).forEach(([key,value])=>{
-  var row=tbody.append("tr");
-  row.text(`${key}: ${value}`);
-  // row.append('td').text(key.concat(":",value));
-});
-});
+    //Create Data Table
+    //Get a refernce to the table body
+    var tbody=d3.select("tbody");
+    tbody.html("")
+    console.log(metadata);
+    // var cur_metadata=metadata.filter(x=>x.id==sample_id);
+    // console.log(cur_metadata);
+    Object.entries(metadata).forEach(([key,value])=>{
+      var row=tbody.append("tr");
+      row.text(`${key}: ${value}`);
+      // row.append('td').text(key.concat(":",value));
+    });
+  });
 };
 
 // buildPlot();
 
-function BuildPlot(id) {
-  console.log(id);
-}
+// function BuildPlot(id) {
+//   console.log(id);
+// }
 
-function updatePlotly() {
-  var tbody=d3.select("tbody");
-  tbody.text("");
-  d3.json('samples.json').then((data)=> {
-  var result_filter= data.samples.filter(x=>x.id==sample_id)[0]
-  // console.log(result_filter);
-  var metadata=data['metadata']; 
-  // console.log(metadata);
-  //use data.samples for bubble chart
-  var otu_ids=data.samples[0]['otu_ids']
-  // console.log(otu_ids);
-  //use samples key, index 0, grab samples_values key
-  var samples = data.samples[0]['sample_values']
-  // console.log(samples)
-  //use samples key, index 0, grab otu_labels
-  var hover_text=data.samples[0]['otu_labels']
-  // console.log(hover_text);
+// function updatePlotly() {
+//   var tbody=d3.select("tbody");
+//   tbody.text("");
+//   d3.json('samples.json').then((data)=> {
+//   var result_filter= data.samples.filter(x=>x.id==sample_id)[0]
+//   // console.log(result_filter);
+//   var metadata=data['metadata']; 
+//   // console.log(metadata);
+//   //use data.samples for bubble chart
+//   var otu_ids=data.samples[0]['otu_ids']
+//   // console.log(otu_ids);
+//   //use samples key, index 0, grab samples_values key
+//   var samples = data.samples[0]['sample_values']
+//   // console.log(samples)
+//   //use samples key, index 0, grab otu_labels
+//   var hover_text=data.samples[0]['otu_labels']
+//   // console.log(hover_text);
 
-  // convert otu_ids to string
-  otu_label= otu_ids.map(x=> `otu ${x}`).slice(0,10).reverse()
+//   // convert otu_ids to string
+//   otu_label= otu_ids.map(x=> `otu ${x}`).slice(0,10).reverse()
 
-    Plotly.restyle('bar', 'y',[otu_label].slice(0,10));
-    Plotly.restyle('bar', 'x',[samples].slice(0,10));
-  });
-};
+//     Plotly.restyle('bar', 'y',[otu_label].slice(0,10));
+//     Plotly.restyle('bar', 'x',[samples].slice(0,10));
+//   });
+// };
+
+
+buildPlot('940');
+
+
+
 
 // updatePlotly()
 
@@ -189,4 +199,3 @@ function updatePlotly() {
 
 // };
 // updatePlotly()
-buildPlot();
